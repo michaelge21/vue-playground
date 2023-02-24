710c652f471fc50858948ffc58f05d9482f3abf9
Michael Ge <michaelge@wustl.edu>
2023-02-23 20:11:18 -0600

    practicing passing props

---
+++ prop-passing/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <section>
+      <Child :age="10" :name="'Bruh...'" :yesProp="true"></Child>
+      <Child :age="100" :name="'second name'"></Child>
+    </section>
+  </main>
+</template>
+
+<script>
+import Child from "./components/Child.vue";
+
+export default {
+  name: "App",
+  components: {
+    Child,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  section {
+    max-width: 1024px;
+    background-color: #222222;
+    color: white;
+    padding: 5vw;
+
+    display: flex;
+    justify-content: space-around;
+
+    section {
+      flex-basis: 300px;
+
+      display: flex;
+      place-items: center;
+      gap: 20px;
+    }
+  }
+}
+</style>
---
+++ prop-passing/src/components/Child.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <p>{{ age }}</p>
+    <p>{{ name }}</p>
+    <p>{{ description.favSong }}</p>
+    <p>{{ description.favPerson }}</p>
+    <p v-if="yesProp">you should only see this if the yesProp is "true"</p>
+    <p v-else>you'll see this if yesProp is false</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  props: {
+    age: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+    description: {
+      type: Object,
+      default(theProp) {
+        return {
+          favSong: "your favorite song goes here!",
+          favPerson: "your favorite person goes here!",
+        };
+      },
+    },
+    yesProp: {
+      type: Boolean,
+      default() {
+        return false;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
